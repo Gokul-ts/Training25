87da7a2799673f1413f38bea70dec600f693fd17
Gokul <Gokul.Thirumasanakumar@trumpf.com>
2025-09-18 12:13:47 +0530

    A1: Intial implementation

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,6 +8,33 @@
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      Console.WriteLine ("*****Number guessing game*****\n");
+      int rNum = new System.Random ().Next (1, 101);
+      GuessNumber (rNum);
+   }
+   static void GuessNumber (int rNum) {
+      int attempts = 0;
+      for (; ; ) {
+         Console.Write ("Enter a number between 1-100: ");
+         string? guess = Console.ReadLine ();
+         if (guess != null) {
+            attempts++;
+            if (int.TryParse (guess, out int inputNum)) {
+               if (inputNum is < 1 or > 100) {
+                  Console.WriteLine ("Please enter between 1 and 100!!!");
+                  continue;
+               }
+               if (inputNum != rNum)
+                  Console.WriteLine ($"Your guess is {(inputNum < rNum ? "lower" : "higher")}");
+               else {
+                  Console.ForegroundColor = ConsoleColor.Green;
+                  Console.WriteLine ("Your guess is correct");
+                  Console.ResetColor ();
+                  break;
+               }
+            } else Console.WriteLine ("Please enter a valid number!!!");
+         } else { Console.WriteLine ("\nFailed to read input"); break; }
+      }
+      Console.WriteLine ($"Attempts taken: {attempts}");
    }
 }
