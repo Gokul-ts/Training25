899446cb944e12025f8aee49e3645495c768b1f6
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-09-23 14:21:10 +0530

    T6: Initial implementation

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,30 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to print digital root of a number.
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      for (; ; ) {
+         Console.Write ("Enter a positive integer: ");
+         string? input = Console.ReadLine ();
+         if (int.TryParse (input, out int num) && num > 0)
+            Console.WriteLine ($"Input: {input}\nDigitalRoot: {DigitalRoot (num)}\n");
+         else
+            Console.WriteLine ("Please enter a valid number!!!\n");
+      }
+   }
+   /// <summary>Returns the digital root of a number</summary>
+   static int DigitalRoot (int num) {
+      while (num > 9) {
+         int sum = 0;
+         while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+         }
+         num = sum;
+      }
+      return num;
    }
 }
