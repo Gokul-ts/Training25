c0e765f727951e3487c4c224203ca97819124180
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-09-23 12:23:02 +0530

    T5: Initial implementation

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,20 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to Multiplication tables from 1-10.
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      Console.WriteLine ("Multiplication tables (1-10):\n");
+      for (int i = 1; i <= 10; i += 2) {
+         PrintTableTwice (i);
+         Console.WriteLine ();
+      }
+   }
+   /// <summary>Prints multiplication table with input num and num+1</summary>
+   static void PrintTableTwice (int num) {
+      for (int i = 1; i <= 10; i++)
+         Console.WriteLine ($"{num} * {i,2} = {num * i} \t {num + 1} * {i,2} = {(num + 1) * i}");
    }
 }
