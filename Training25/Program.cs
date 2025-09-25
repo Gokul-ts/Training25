ba55f698df0ee797a71810938e812df4726ba2c6
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-09-25 16:29:08 +0530

    T11: Initial implementation

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,31 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to check Armstrong number.
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      CheckNum ();
+   }
+
+   /// <summary>Checks whether number is Armstrong or not </summary>
+   static void CheckNum () {
+      for (; ; ) {
+         Console.Write ("Enter a number: ");
+         var input = Console.ReadLine ();
+         if (int.TryParse (input, out int num) && num >= 0) {
+            Console.WriteLine ($"It is {(IsArmstrong (num) ? "" : "not ")}an Armstrong number");
+         } else Console.WriteLine ("Please enter a valid number!!");
+      }
+
+      bool IsArmstrong (int num) {
+         double sum = 0, org = num, pow = num.ToString ().Length;
+         while (num > 0) {
+            sum += Math.Pow (num % 10, pow);
+            num /= 10;
+         }
+         return sum == org;
+      }
    }
 }
