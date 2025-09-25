276f3bd02b9f1298ec51c3c3eecdfe875b96a26b
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-09-25 10:52:17 +0530

    T9: Initial implementation

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,6 +8,22 @@
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      ReducedString ();
+   }
+
+   /// <summary>Removes adjacent pair of lowercase letters from a string</summary>
+   static void ReducedString () {
+      for (; ; ) {
+         Console.Write ("Enter a string input: ");
+         var input = Console.ReadLine ()?.ToLower ();
+         if (!string.IsNullOrEmpty (input) && !input.Any (a => char.IsDigit (a) || a is ' ')) {
+            int i = 0;
+            while (i < input.Length - 1) {
+               if (input[i] == input[i + 1]) input = input.Remove (i, 2);
+               else i++;
+            }
+            Console.WriteLine ($"Output: {(input.Length is 0 ? "Empty string" : input)}");
+         } else Console.WriteLine ("Please enter a valid input!!");
+      }
    }
 }
