f600c7afa30d2f6fc597b4a67aed199f25266396
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-09-26 09:50:43 +0530

    T11.1: Initial implementation

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,44 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to print the nth Armstrong number.
 // ------------------------------------------------------------------------------------------------
+using System; //Required to run program in terminal
+
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      PrintArmsNum (args);
+   }
+
+   /// <summary>Prints the nth Armstrong number</summary>
+   static void PrintArmsNum (string[] args) {
+      int n = args.Length;
+      if (n is 0)
+         Console.Write ("Enter a number between 1 and 25: ");
+      var input = n is 0 ? Console.ReadLine () : args[0];
+      if (int.TryParse (input, out int num) && num is > 0 and <= 25) {
+         Console.WriteLine ($"Armstrong num: {nArmstrong (num)}");
+      } else Console.WriteLine ("Please enter a valid number!!");
+   }
+
+   /// <summary>Returns whether a number is Armstrong or not</summary>
+   static bool IsArmstrong (int num) {
+      double sum = 0, org = num, pow = num.ToString ().Length;
+      while (num > 0) {
+         sum += Math.Pow (num % 10, pow);
+         num /= 10;
+      }
+      return sum == org;
+   }
+
+   /// <summary>Returns the nth Armstrong number</summary>
+   static int nArmstrong (int n) {
+      int count = 0, num = 0;
+      while (count < n) {
+         if (IsArmstrong (num)) count++;
+         num++;
+      }
+      return num - 1;
    }
 }
