c756163f83625cd1be19598f5458ccb7081edd1c
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-09-26 11:45:17 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -6,36 +6,37 @@
 // Program to validate a password.
 // ------------------------------------------------------------------------------------------------
 using System.Text;
+
 namespace Training25;
 internal class Program {
    static void Main () {
-      CheckValidity ();
+      ValidatePassword ();
    }
 
-   /// <summary>Checks the validity of password and prints result</summary>
-   static void CheckValidity () {
+   /// <summary>Checks the validity of password and prints the result. The password should 
+   /// have atleast 1 digit, 1 uppercase, 1 lowercase, 1 special character and 6 characters. 
+   /// If it satisfies all criteria the password is strong else weak.</summary>
+   static void ValidatePassword () {
       for (; ; ) {
          Console.Write ("Enter password: ");
          var input = Console.ReadLine ();
-         if (string.IsNullOrEmpty (input) || input.Any (a => a == ' ')) {
+         if (string.IsNullOrEmpty (input) || input.Any (a => a is ' ')) {
             Console.WriteLine ("Please enter a valid password!!!\n");
             continue;
          }
          Console.ForegroundColor = ConsoleColor.DarkRed;
-         var result = new StringBuilder ("Your password is weak.\nIt should have atleast");
+         var result = new StringBuilder ();
          string spChars = "!@#$%^&*()-+";
-         bool isStrong = true;
          if (input.Length >= 6) {
-            if (!input.Any (a => char.IsDigit (a))) { result.Append (" 1 digit"); isStrong = false; }
-            if (!input.Any (a => char.IsUpper (a))) { result.Append (" 1 upper case"); isStrong = false; }
-            if (!input.Any (a => char.IsLower (a))) { result.Append (" 1 lower case"); isStrong = false; }
-            if (!spChars.Any (a => input.Contains (a))) { result.Append (" 1 special character"); isStrong = false; }
-         } else { result.Append (" 6 characters"); isStrong = false; }
-         if (isStrong) {
+            if (!input.Any (char.IsDigit)) result.Append (" 1 digit");
+            if (!input.Any (char.IsUpper)) result.Append (" 1 upper case");
+            if (!input.Any (char.IsLower)) result.Append (" 1 lower case");
+            if (!spChars.Any (input.Contains)) result.Append (" 1 special character");
+         } else result.Append (" 6 characters");
+         if (result.Length is 0) {
             Console.ForegroundColor = ConsoleColor.Green;
-            result.Clear ();
             result.Append ("Your password is strong");
-         }
+         } else result.Insert (0, "Your password is weak.\nIt should have atleast");
          Console.WriteLine (result + "\n");
          Console.ResetColor ();
       }
