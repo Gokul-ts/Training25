661ddac586419c7898537df44a03af7bf261760f
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-07 15:59:31 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,32 +3,35 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program to print a sorted array with special characters at the end.
+// Program takes only character input from user and prompts the user for a special character 
+// and order for sorting. If nothing is entered ascending order is taken as default. Prints the 
+// sorted array with special character at the end.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 internal class Program {
-   static void Main () => SortAndSwap ();
-
-   /// <summary>Function takes only character input from user and prompts the user for a special character 
-   /// and order for sorting. If nothing is entered ascending order is taken as default. Prints the 
-   /// sorted array to the console with special character at the end. Special character and input string
-   /// are not case sensitive but should not be empty.</summary>
-   static void SortAndSwap () {
+   static void Main () {
       for (; ; ) {
-         Console.Write ("Enter the character array: ");
-         var inpArr = Console.ReadLine ();
-         Console.Write ("Enter special character: ");
-         var spChar = Console.ReadKey ().KeyChar;
-         Console.Write ("\nEnter [A]scending or [D]escending: ");
-         var order = Console.ReadKey ().Key;
-         if (!string.IsNullOrEmpty (inpArr) && inpArr.All (char.IsLetter) &&
-            char.IsLetter (spChar) && order is ConsoleKey.D or ConsoleKey.A or ConsoleKey.Enter) {
-            var spChars = inpArr.Where (a => a == char.ToUpper (spChar) || a == char.ToLower (spChar));
-            var sortArr = inpArr.Where (a => !spChars.Contains (a));
-            sortArr = order is ConsoleKey.D ? sortArr.OrderDescending () : sortArr.Order ();
-            sortArr = sortArr.Concat (spChars.Any () ? spChars : Enumerable.Empty<char> ());
-            Console.WriteLine ("\nSorted array: " + string.Concat (sortArr));
-         } else Console.WriteLine ("\nPlease enter a valid input!!");
+         Write ("Enter the character array: ");
+         var inpArr = ReadLine ();
+         Write ("Enter special character: ");
+         var spChar = ReadKey ().KeyChar;
+         Write ("\nEnter [A]scending or [D]escending: ");
+         var order = ReadKey ().Key;
+         WriteLine ((!string.IsNullOrEmpty (inpArr) && inpArr.All (char.IsLetter) && char.IsLetter (spChar) &&
+            order is ConsoleKey.D or ConsoleKey.A or ConsoleKey.Enter) ?
+            $"\nSorted array: {SortAndSwap (inpArr, spChar, order)}" :
+            "\nPlease enter a valid input!!");
+      }
+
+      /// <summary>Returns the sorted array with special character at the end.</summary>
+      string SortAndSwap (string inpArr, char spChar, ConsoleKey order) {
+         var spChars = inpArr.Where (a => a == char.ToUpper (spChar) || a == char.ToLower (spChar));
+         var sortArr = inpArr.Where (a => !spChars.Contains (a));
+         sortArr = order is ConsoleKey.D ? sortArr.OrderDescending () : sortArr.Order ();
+         sortArr = sortArr.Concat (spChars.Any () ? spChars : Enumerable.Empty<char> ());
+         return string.Concat (sortArr);
       }
    }
 }
