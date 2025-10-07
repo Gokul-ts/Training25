0e820079b25affce4ffd065599b6e29b223f4f78
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-07 15:40:40 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,27 +5,28 @@
 // Program.cs
 // Program to find the winner of a contest given a string of votes.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 internal class Program {
    static void Main () {
-      FindWinner ();
-   }
-
-   /// <summary>Prints the winner of a contest with the maximum number of votes 
-   /// or the first one to get max. votes if results are tied</summary>
-   static void FindWinner () {
       for (; ; ) {
-         Console.Write ("Enter a string: ");
-         var input = Console.ReadLine ();
-         if (!string.IsNullOrEmpty (input) && input.All (char.IsLetter)) {
-            Dictionary<char, int> voteData = new ();
-            string votes = input.ToUpper ();
-            foreach (char c in votes)
-               if (voteData.Keys.Contains (c)) voteData[c]++;
-               else voteData.Add (c, 1);
-            var winners = voteData.Where (a => a.Value == voteData.Values.Max ());
-            Console.WriteLine ($"Winner: {winners.First ().Key} Votes: {winners.First ().Value}");
-         } else Console.WriteLine ("Please enter a valid input");
+         Write ("Enter a string: ");
+         var input = ReadLine ();
+         WriteLine (!string.IsNullOrEmpty (input) && input.All (char.IsLetter) ?
+            $"Winner: {FindWinner (input).Key} Votes: {FindWinner (input).Value}" :
+            "Please enter a valid input");
+      }
+
+      /// <summary>Returns the winner with maximum number of votes.</summary> 
+      static KeyValuePair<char, int> FindWinner (string input) {
+         Dictionary<char, int> voteData = new ();
+         string votes = input.ToUpper ();
+         foreach (char c in votes)
+            if (voteData.Keys.Contains (c)) voteData[c]++;
+            else voteData.Add (c, 1);
+         var winners = voteData.Where (a => a.Value == voteData.Values.Max ());
+         return winners.First ();
       }
    }
 }
