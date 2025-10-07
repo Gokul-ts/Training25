bc47a8bcc0c3d726cd5353dbaf9c96c3c01463d2
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-07 16:21:21 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,30 +5,32 @@
 // Program.cs
 // Program to calculate the smallest number of steps to transform a number.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 internal class Program {
-   static void Main (string[] args) => Transform ();
-
-   /// <summary>Takes only integer input from user and filters out the distinct elements.
-   /// Returns the minimum no. of steps required to transform the integer into identical digits.</summary>
-   static void Transform () {
+   static void Main (string[] args) {
       for (; ; ) {
-         Console.Write ("Enter a number: ");
-         var input = Console.ReadLine ();
-         if (int.TryParse (input, out int num) && num > 0) {
-            int minSteps = int.MaxValue;
-            var digits = input.Distinct ().Select (c => c - '0').ToList ();
-            foreach (int target in digits) {
-               int temp = num, steps = 0;
-               while (temp > 0) {
-                  int digit = temp % 10;
-                  steps += digit > target ? digit - target : target - digit;
-                  temp /= 10;
-               }
-               if (steps < minSteps) minSteps = steps;
+         Write ("Enter a number: ");
+         var input = ReadLine ();
+         WriteLine (int.TryParse (input, out int num) && num > 0 ?
+            $"Minimum steps to transform: {Transform (num)}" :
+            "Please enter a valid number!!");
+      }
+
+      /// <summary>Returns the minimum no. of steps required to transform an integer.</summary>
+      int Transform (int num) {
+         int minSteps = int.MaxValue;
+         var digits = num.ToString ().Distinct ().Select (c => c - '0').ToList ();
+         foreach (int target in digits) {
+            int temp = num, steps = 0;
+            for (; temp > 0; temp /= 10) {
+               int digit = temp % 10;
+               steps += digit > target ? digit - target : target - digit;
             }
-            Console.WriteLine ($"Minimum steps to transform: {minSteps}\n");
-         } else Console.WriteLine ("Please enter a valid number!!\n");
+            if (steps < minSteps) minSteps = steps;
+         }
+         return minSteps;
       }
    }
 }
