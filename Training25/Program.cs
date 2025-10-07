3c999711ef8341bafdbfde607f40825a08d5d465
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-07 10:50:52 +0530

    Review fix 3

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -12,16 +12,24 @@ internal class Program {
          Console.Write ("Enter a number or [X] to exit: ");
          var input = Console.ReadLine ();
          if (input?.ToUpper () == "X") Environment.Exit (0);
-         Console.WriteLine (int.TryParse (input, out int num) && num >= 0 ?
-         $"Input: {num}\nHEX: {num:X}\nBinary: {Binary (num)}" :
-         "Please enter a valid input!!!");
+         if (int.TryParse (input, out int num) && num >= 0)
+            Console.WriteLine ($"Input: {num}" + Environment.NewLine + $"HEX: {Hex (num)}"
+               + Environment.NewLine + $"Binary: {Binary (num)}");
+         else Console.WriteLine ("Please enter a valid input!!!");
       }
 
       /// <summary>Returns the binary value of a number</summary>
       string Binary (int num) {
          string result = num is 0 ? "0" : string.Empty;
-         for (; num > 0; num /= 2) result = (num % 2) + result;
+         for (; num > 0; num /= 2) result = $"{num % 2}{result}";
          return result;
+      }
+
+      /// <summary>Returns the hexadecimal value of a number</summary>
+      string Hex (int num) {
+         string hex = "0123456789ABCDEF", result = string.Empty;
+         for (; num > 0; num /= 16) result = $"{hex[num % 16]}{result}";
+         return result is "" ? "0" : result;
       }
    }
 }
