5f7158244688c2b35414fcb5a086d9d7d1c40f5f
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-08 09:24:33 +0530

    Review fix 3

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,12 +3,14 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program to Multiplication tables from 1-10.
+// Program to print Multiplication tables from 1-10.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 internal class Program {
    static void Main () {
-      Console.WriteLine ("Multiplication tables (1-10):\n");
+      WriteLine ("Multiplication tables (1-10):\n");
       for (int i = 1; i <= 10; i += 2)
          PrintTableTwice (i);
    }
@@ -16,6 +18,7 @@ internal class Program {
    /// <summary>Prints multiplication table with input num and num+1</summary>
    static void PrintTableTwice (int num) {
       for (int i = 1; i <= 10; i++)
-         Console.WriteLine ($"{num} * {i,2} = {num * i} \t {num + 1} * {i,2} = {(num + 1) * i}{(i == 10 ? "\n" : "")}");
+         WriteLine ($"{num} * {i,2} = {num * i} \t {num + 1} * {i,2} = {(num + 1) * i}");
+      WriteLine ();
    }
 }
