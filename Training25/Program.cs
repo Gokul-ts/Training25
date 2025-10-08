5fd9f8f260d61ff1d3add48be7ffdc39276b0483
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-08 13:48:50 +0530

    Review fix 3

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -20,21 +20,21 @@ internal class Program {
       string[] whitePieces = { "\u2656", "\u2658", "\u2657", "\u2655", "\u2654" },
                blackPieces = { "\u265C", "\u265E", "\u265D", "\u265B", "\u265A" };
       string whitePawn = "\u2659", blackPawn = "\u265F";
-      WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
+      WriteLine ("┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓");
       for (int row = 0; row < 8; row++) {
          Write ("┃");
          for (int col = 0; col < 8; col++) {
             string cellContent = row switch {
-               0 => $" {(col > 4 ? blackPieces[7 - col] : blackPieces[col])} ┃",
-               1 => $" {blackPawn} ┃",
-               6 => $" {whitePawn} ┃",
-               7 => $" {(col > 4 ? whitePieces[7 - col] : whitePieces[col])} ┃",
-               _ => "   ┃"
+               0 => col > 4 ? blackPieces[7 - col] : blackPieces[col],
+               1 => blackPawn,
+               6 => whitePawn,
+               7 => col > 4 ? whitePieces[7 - col] : whitePieces[col],
+               _ => " "
             };
-            Write (cellContent);
+            Write ($" {cellContent}  ┃");
          }
-         if (row < 7) Write ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫\n");
+         if (row < 7) Write ("\n┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫\n");
       }
-      Write ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛\nLet's play chess!! \u265B");
+      Write ("\n┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛\nLet's play chess!! \u265B");
    }
 }
