2fb4b8b3e8d19d07199f1e3ca31ff81212e5bd7b
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-09 16:27:53 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -12,20 +12,19 @@ internal class Program {
    static void Main () {
       for (; ; ) {
          Write ("Enter a number: ");
-         var input = ReadLine ();
-         WriteLine (int.TryParse (input, out int num) && (num >= 0) ?
+         WriteLine (int.TryParse (ReadLine (), out int num) && (num >= 0) ?
             $"It is {(IsArmstrong (num) ? "" : "not ")}an Armstrong number" :
             "Please enter a valid number!!");
       }
+   }
 
-      /// <summary>Returns whether the number is Armstrong or not </summary>
-      bool IsArmstrong (int num) {
-         double sum = 0, org = num, pow = num.ToString ().Length;
-         while (num > 0) {
-            sum += Math.Pow (num % 10, pow);
-            num /= 10;
-         }
-         return sum == org;
+   /// <summary>Returns whether the number is Armstrong or not </summary>
+   static bool IsArmstrong (int num) {
+      double sum = 0, org = num, pow = num.ToString ().Length;
+      while (num > 0) {
+         sum += Math.Pow (num % 10, pow);
+         num /= 10;
       }
+      return sum == org;
    }
 }
