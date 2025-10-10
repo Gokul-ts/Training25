72f19a6bbe3b83934d27a5f8bc5c217e8c6535f8
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-10-10 11:18:40 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -11,32 +11,34 @@ using static System.Console;
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      int n = args.Length;
-      if (n is 0)
+      var input = "";
+      if (args.Length == 0) {
          Write ("Enter a number between 1 and 25: ");
-      var input = n is 0 ? ReadLine () : args[0];
-      WriteLine (int.TryParse (input, out int num) && num is > 0 and <= 25 ?
+         input = ReadLine ();
+      } else input = args[0];
+      WriteLine (int.TryParse (input, out int num) && num >= 0 && num <= 25 ?
          $"Armstrong num: {NthArmstrong (num)}" :
          "Please enter a valid number!!");
+   }
 
-      /// <summary>Returns whether a number is Armstrong or not</summary>
-      bool IsArmstrong (int num) {
-         double sum = 0, org = num, pow = num.ToString ().Length;
-         while (num > 0) {
-            sum += Math.Pow (num % 10, pow);
-            num /= 10;
-         }
-         return sum == org;
+   /// <summary>Returns whether a number is Armstrong or not</summary>
+   static bool IsArmstrong (int num) {
+      double sum = 0, org = num, pow = 0;
+      for (int temp = num; temp > 0; temp /= 10) pow++;
+      while (num > 0) {
+         sum += Math.Pow (num % 10, pow);
+         num /= 10;
       }
+      return sum == org;
+   }
 
-      /// <summary>Returns the nth Armstrong number</summary>
-      int NthArmstrong (int n) {
-         int count = 0, num = 0;
-         while (count < n) {
-            if (IsArmstrong (num)) count++;
-            num++;
-         }
-         return num - 1;
+   /// <summary>Returns the nth Armstrong number</summary>
+   static int NthArmstrong (int n) {
+      int count = 0, num = 0;
+      while (count < n) {
+         if (IsArmstrong (num)) count++;
+         num++;
       }
+      return --num;
    }
 }
